8c3f9eeb803825f777a64a30b352b8165b92479c
peng4740 <35738277+peng4740@users.noreply.github.com>
2021-03-24 15:22:34 +0800

    Update besttace.sh

--- besttace.sh
+++ besttace.sh
@@ -1,20 +1,11 @@
-#!/bin/sh
-dir=/tmp/besttrace4linux
-file=$dir/besttrace4linux.zip
-file2=$dir/besttrace
-mkdir -p $dir
-wget https://cdn.ipip.net/17mon/besttrace4linux.zip -O $file
-unzip $file -d $dir
-chmod +x $file2
-/bin/mv -f $file2 /usr/local/bin/
-rm -rf $dir
-if [ "$(command -v /usr/local/bin/besttrace)" ]; then besttrace='/usr/local/bin/besttrace'; fi
-if [ "$(command -v besttrace)" ]; then besttrace='besttrace'; fi
-if [ "$besttrace" ]; then
+#!/bin/bash
+echo_help(){
+besttrace=$1
 red='\e[31m'
 non='\e[0m'
 clear
 echo -e "执行${red}${besttrace} 域名/ip${non}开始运行
+执行${red}${besttrace} -T 域名/ip${non}是测试TCP路由，否则是ICMP路由
 例子如下：
 北京电信:${red}${besttrace} 113.59.224.1${non}
 北京联通:${red}${besttrace} 61.49.137.5${non}
@@ -26,6 +17,37 @@ echo -e "执行${red}${besttrace} 域名/ip${non}开始运行
 广州联通:${red}${besttrace} 27.40.0.30${non}
 广州移动:${red}${besttrace} 221.183.63.121${non}
 成都教育网:${red}${besttrace} 202.112.14.151${non}"
+}
+
+dir=/tmp/besttrace4linux
+dir2=/bin
+[ "$(echo $PATH|grep /usr/local/bin)" ] && dir2=/usr/local/bin
+
+#判断是否安装过了
+[ "$(command -v $dir2/besttrace)" ] && besttrace=$dir2/besttrace && [ "$(command -v besttrace)" ] && besttrace=besttrace && [ "$(command -v trace)" ] && besttrace=trace
+if [ "$besttrace" ]; then
+echo_help "$besttrace"
+exit 0 #安装过了就退出脚本
+fi
+file=$dir/besttrace4linux.zip
+file2=$dir/besttrace
+loc=$(curl -s http://104.19.19.19/cdn-cgi/trace|grep loc|cut -d '=' -f 2)
+echo "Location:$loc"
+if [[ -z "$loc" || "$loc" == "CN" ]]; then
+url="https://cdn.ipip.net/17mon/besttrace4linux.zip"
+else
+url="https://raw.githubusercontent.com/peng4740/besttrace.sh/main/file/besttrace4linux.zip"
+fi
+mkdir -p $dir
+wget $url -O $file
+unzip $file -d $dir
+chmod +x $file2
+mv $file2 $dir2
+ln -s $dir2/besttrace $dir2/trace
+rm -rf $dir
+[ "$(command -v $dir2/besttrace)" ] && besttrace=$dir2/besttrace && [ "$(command -v besttrace)" ] && besttrace=besttrace && [ "$(command -v trace)" ] && besttrace=trace
+if [ "$besttrace" ]; then
+echo_help "$besttrace"
 else
 echo -e "${red}好像安装失败了${non} -_-!"
 fi
